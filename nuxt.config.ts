115d74eb8b6fa50eec6460d66688df64f7486e84
Ronald Punt <ronald.punt@mac.com>
2023-08-06 21:10:24 +0200

    better now?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  ssr:false,
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     viewer: true,
