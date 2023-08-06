229c5413f6d21301c4d6af7d7702368130534ca8
Ronald Punt <ronald.punt@mac.com>
2023-08-06 21:49:58 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
-  router: {
-    base: '/el-static-2024/'
+  generate:{
+    nojekyll: true, //not working on this version
+    fallback: '404.html',
   },
+  ssr: false, //When false
+  target: 'static',
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     viewer: false,
+  },
+  app: {
+    baseURL: "/el-static-2024/"
   },
   devtools: { enabled: true }
 })
